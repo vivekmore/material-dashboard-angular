5386d64374a8fff9a28707eab5641fac65f5077d
Vivek More <vivek.more@gfs.com>
2017-09-17 08:09:07 -0400

    update Customer model

--- src/app/table-list/customer.model.ts
+++ src/app/table-list/customer.model.ts
@@ -1,7 +1,15 @@
 interface Customer {
     _id: string;
     name: Name;
-    address: string;
+    address: Address;
     phone: string;
     email: string;
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    city: string;
+    state: string;
+    zip: string;
 }
